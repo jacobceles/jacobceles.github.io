f7782cbcd503bf234df4d2512fdfb319af1c2f89
Jacob Celestine <jacobceles@gmail.com>
2020-04-12 22:01:47 +0530

    Updated skill bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,5 @@
-jQuery(document).ready(function($) {
-
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
-    });
-   
-    
-
-});
+// Skills Bars
+$('.skill-percent').each(function(){
+  $(this).animate({
+    width:$(this).attr('data-percent')},"fast");
+  });
